8be3462c3793e0fb4f028e1c488531feb399ed78
Ilya Ivanov <ilya@devopsx.com>
2024-08-05 16:35:01 +0700

    Add Sentry integration

--- arlo-downloader.py
+++ arlo-downloader.py
@@ -9,6 +9,25 @@ import argparse
 
 from config import Config
 
+# Optionally, enable Sentry. Should be initialized as early as possible.
+SENTRY_DSN = os.getenv("SENTRY_DSN", "_invalid")
+if SENTRY_DSN != "_invalid":
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
+    print("Sentry enabled.")
+else:
+    print("Sentry disabled.")
+
 
 def parse_arguments():
 
